7fd6e9aae8760dbf3c211ee39d717d4cb7a4cc6d
Sol Leon <melon@kix.com.ec>
2026-01-14 15:10:33 -0500

    backend-delivery sol

--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaMariaDb({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST!,
+      user: process.env.DATABASE_USER!,
+      password: process.env.DATABASE_PASSWORD!,
+      database: process.env.DATABASE_NAME!,
     });
+
     super({ adapter });
   }
 }
